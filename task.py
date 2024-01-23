1a97ccba2ad33336e8653285208831ac67266f45
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-01-23 17:48:21 -0300

    Feat iFlavor 1st commit

---
+++ task.py
@@ -0,0 +1,17 @@
+print('Python na Escola de programação da Alura')
+
+nome_user = input('Qual o seu nome?')
+idade_user = input('Qual a sua idade?')
+
+print(f'Meu nome é {nome_user} e tenho {idade_user}')
+
+print('''
+      A
+      L
+      U
+      R
+      A
+      ''')
+
+pi = 3.14159
+print(f'O valor arrendondado de pi é: {pi:.2f}')
